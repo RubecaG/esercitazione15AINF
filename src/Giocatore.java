4372de155f0695caee19b389992c617cca1f1d70
RubecaG <rubecagiacomo@gmail.com>
2025-10-22 21:07:23 +0200

    Update Giocatore.java

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
 public class Giocatore extends Thread {
     private final String nome;
     private final GestoreGioco gestore;
-    private static final Scanner input = new Scanner(System.in);
+    private static final Object lock = new Object(); // oggetto condiviso per la sincronizzazione
 
     public Giocatore(String nome, GestoreGioco gestore) {
         this.nome = nome;
@@ -15,12 +13,22 @@ public class Giocatore extends Thread {
     }
 
     public void gioca() {
-        System.out.print(nome + ", inserisci un numero: ");
-        int numeroScelto = input.nextInt();
+        synchronized (lock) {
+            System.out.println(nome + " sta scegliendo un numero...");
 
-        boolean haVinto = gestore.verifica(nome, numeroScelto);
-        if (haVinto) {
-            System.out.println(nome + " ha vinto la partita!");
+            try {
+                Thread.sleep(500); // piccola pausa per simulare il pensiero
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+
+            int numeroScelto = (int) (Math.random() * 30) + 1;
+            System.out.println(nome + " ha scelto il numero: " + numeroScelto);
+
+            boolean haVinto = gestore.verifica(nome, numeroScelto);
+            if (haVinto) {
+                System.out.println(nome + " ha vinto la partita!");
+            }
         }
     }
 
